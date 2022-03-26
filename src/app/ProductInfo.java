db70381576647c15136a8765d16b2de44c774eba
soldat7414 <soldat7414@gmail.com>
2022-03-26 11:32:28 +0200

    Add class ProductInfo.java add in class add his fields

---
+++ src/app/ProductInfo.java
@@ -0,0 +1,13 @@
+package app;
+
+public class ProductInfo {
+    // Переменные этого класса.
+    // Они задекларированы здесь.
+    // Не путать с переменными класса-модели.
+    static String name;
+    static double weight;
+    static int quantity;
+    static Product product;
+    static double totalWeight;
+    static private final String MEASURE = "кг";
+}
