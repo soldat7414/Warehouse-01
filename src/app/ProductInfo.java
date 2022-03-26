ec52c2c4f990043adff3104ebf5e30882dd74894
soldat7414 <soldat7414@gmail.com>
2022-03-26 11:37:39 +0200

    Add static method doInputs in class ProductInfo

--- src/app/ProductInfo.java
+++ src/app/ProductInfo.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.Scanner;
+
 public class ProductInfo {
     // Переменные этого класса.
     // Они задекларированы здесь.
@@ -10,4 +12,14 @@ public class ProductInfo {
     static Product product;
     static double totalWeight;
     static private final String MEASURE = "кг";
+
+
+    static private void doInputs(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Наименование товара: ");
+        name = scanner.nextLine();
+        System.out.print("Количество товара (шт.): ");
+        quantity = scanner.nextInt();
+        scanner.close();
+    }
 }
