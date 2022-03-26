1d73463b87a2315fe6c1557f0db2304f8e5a30aa
soldat7414 <soldat7414@gmail.com>
2022-03-26 11:12:28 +0200

    Add the model of product object fields: String name; double weight; getters to this fields

--- src/app/Product.java
+++ src/app/Product.java
@@ -1,5 +1,25 @@
 package app;
 
-
+// Модель объекта товара.
+// Модель рассматриваем как
+// абстрактное представление
+// единицы товара.
 public class Product {
+    // Свойства единицы товара: имя, вес.
+    // Количество мы заранее не знаем.
+    private final String name;
+    private final double weight;
+
+    public Product (String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
 }
